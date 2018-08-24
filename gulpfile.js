49c1fdde6d91d0079775a33ca0f01e4fed2b24d4
Darkmus <darkmus@tut.by>
2018-08-24 10:19:23 +0300

    Optimize loading of fonts, menu js and other. Check crossbrowsing

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var svgstore = require("gulp-svgstore");
 var rename = require("gulp-rename");
+var mincss = require("gulp-csso");
 var imagemin = require("gulp-imagemin");
 var imageminJpegRecompress = require('imagemin-jpeg-recompress');
 var pngquant = require('imagemin-pngquant');
@@ -19,16 +20,36 @@ var gulpAutoprefixer = require('gulp-autoprefixer');
 var jsfiles = require('./jsfiles.json');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var htmlmin = require("gulp-htmlmin");
+var pump = require('pump');
 
-gulp.task('js', function () {
+gulp.task('jsDev', function () { // Склеивает js модули для разработки
   return gulp.src(jsfiles, {base: 'src/js/modules'})
-      .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(concat('all.min.js'))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('dev/js'));
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dev/js'));
 });
 
-gulp.task('images', function() {
+gulp.task('jsBuild', function () { // Склеивает и сжимает js модули для билда
+  return gulp.src(jsfiles, {base: 'src/js/modules'})
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('jsMinBuild', function (cb) { // Сжимает самостоятельные js файлы для билда
+  pump([
+        gulp.src('src/js/*.js'),
+        uglify(),
+        gulp.dest('build/js')
+    ],
+    cb
+  );
+});
+
+gulp.task('imagesBuild', function() { // Оптимизирует растровую графику (только для билда)
   return gulp.src('src/img/**/*.{jpg,png}')
     .pipe(cache(imagemin([
       imagemin.gifsicle({interlaced: true}),
@@ -42,23 +63,29 @@ gulp.task('images', function() {
       imagemin.optipng({optimizationLevel: 3}),
       pngquant({quality: '50-60', speed: 5})
     ],{
-      // verbose: true
+      verbose: false
     })))
-    .pipe(gulp.dest('dev/img'));
+    .pipe(gulp.dest('build/img'));
 });
 
-gulp.task('clearCache', function (done) {
+gulp.task('clearCache', function (done) { // Чистит кэш
   return cache.clearAll(done);
 });
 
-gulp.task('webp', function() {
-  return gulp.src('src/img/**/*.{jpg,png}', {since: gulp.lastRun('webp')})
+gulp.task('webpDev', function() { // Копирует графику в webP формат для разработки
+  return gulp.src('src/img/**/*.{jpg,png}', {since: gulp.lastRun('webpDev')})
     .pipe(newer('dev/img'))
     .pipe(webp({quality: 70}))
     .pipe(gulp.dest("dev/img"));
 });
 
-gulp.task('style', function () {
+gulp.task('webpBuild', function() { // Копирует графику в webP формат для билда
+  return gulp.src('src/img/**/*.{jpg,png}')
+    .pipe(webp({quality: 70}))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task('styleDev', function () { // Создает из стилей less style.css, расставляет префиксы (разработка)
   return gulp.src('src/less/style.less')
     .pipe(plumber())
     .pipe(less())
@@ -70,14 +97,32 @@ gulp.task('style', function () {
     .pipe(gulp.dest('dev/css'))
 });
 
-gulp.task('copy', function () {
-  return gulp.src('src/{fonts,js}/*.*', {since: gulp.lastRun('copy')})
+gulp.task('styleBuild', function () { // Создает из стилей less style.css, расставляет префиксы, минифицирует (билд)
+  return gulp.src('src/less/style.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulpAutoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+      grid: true
+    }))
+    .pipe(mincss())
+    .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('copyDev', function () { // копирует файлы для разработки
+  return gulp.src('src/{fonts,js,img}/*.*', {since: gulp.lastRun('copyDev')})
   .pipe(newer('dev'))
   .pipe(gulp.dest('dev'))
 });
 
-gulp.task('copyHTML', function () {
-  return gulp.src('src/*.html', {since: gulp.lastRun('copyHTML')})
+gulp.task('copyBuild', function () { // копирует шрифты для билда
+  return gulp.src('src/fonts/*.*')
+  .pipe(gulp.dest('build/fonts'))
+});
+
+gulp.task('copyHTMLDev', function () { // копирует html для разработки
+  return gulp.src('src/*.html', {since: gulp.lastRun('copyHTMLDev')})
   .pipe(newer('dev'))
   .pipe(posthtml([
     include()
@@ -85,11 +130,24 @@ gulp.task('copyHTML', function () {
   .pipe(gulp.dest('dev'))
 });
 
-gulp.task('clean', function () {
+gulp.task('copyHTMLBuild', function () { // копирует и сжимает html для билда
+  return gulp.src('src/*.html')
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest('build'))
+});
+
+gulp.task('cleanDev', function () { // удаляет папку "dev"
   return del('dev');
 });
 
-gulp.task('sprite', function() {
+gulp.task('cleanBuild', function () { // удаляет папку "build"
+  return del('build');
+});
+
+gulp.task('spriteDev', function() { // Оптимизирует SVG и создает спрайт для разработки
   return gulp.src("src/img/icon-*.svg")
     .pipe(imagemin([
       imagemin.svgo()
@@ -101,7 +159,19 @@ gulp.task('sprite', function() {
     .pipe(gulp.dest("dev/img"));
 });
 
-gulp.task('serve', function () {
+gulp.task('spriteBuild', function() { // Оптимизирует SVG и создает спрайт для билда
+  return gulp.src("src/img/icon-*.svg")
+    .pipe(imagemin([
+      imagemin.svgo()
+    ]))
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task('serve', function () { // Запускает сервер, при изменениях перезагружается
   browserSync.init({
     server: 'dev',
     notify: false
@@ -110,10 +180,12 @@ gulp.task('serve', function () {
   browserSync.watch('dev/**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task('watch', function () {
-  gulp.watch('src/**/*.*', gulp.series('style'));
-  gulp.watch('src/{fonts,img,js}/**/*.*', gulp.series('copy'));
-  gulp.watch('src/*.html', gulp.series('copyHTML'));
+gulp.task('watch', function () { // Настройки вотчера
+  gulp.watch('src/**/*.less', gulp.series('styleDev'));
+  gulp.watch('src/{fonts,img,js}/**/*.*', gulp.parallel('copyDev', 'jsDev', 'webpDev'));
+  gulp.watch('src/*.html', gulp.series('copyHTMLDev'));
 });
 
-gulp.task('dev', gulp.series(gulp.parallel('style', 'copy', 'images', 'webp', 'js', gulp.series('sprite', 'copyHTML')), gulp.parallel('clearCache', 'watch', 'serve')));
+gulp.task('dev', gulp.series(gulp.parallel('styleDev', 'copyDev', 'webpDev', 'jsDev', gulp.series('spriteDev', 'copyHTMLDev')), gulp.parallel('clearCache', 'watch', 'serve')));
+
+gulp.task('build', gulp.series('cleanBuild', gulp.parallel('styleBuild', 'imagesBuild', 'copyBuild', 'webpBuild', 'jsBuild', 'jsMinBuild', gulp.series('spriteBuild', 'copyHTMLBuild', 'clearCache'))));
